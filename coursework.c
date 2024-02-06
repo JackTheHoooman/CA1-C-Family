eaca2fcfac255cf5d0b81437a80f1c97c52fb7c1
JackTheHoooman <jackethanbundy@gmail.com>
2024-02-06 15:17:31 +0000

    Additions to compile script

--- coursework.c
+++ coursework.c
@@ -3,10 +3,32 @@
 
 void printBytes(void *ptr, int numBytes);
 
-int main () {
-    char test[5] = {10,2,3,4,5};
+int main (int argc, char **argv) {
+    printf("--- Task 1 ---\n");
+    /* "You should call that function with a couple of different 
+    values from within the main function." */
 
-    printBytes(&test, 4);
+    unsigned char test1[5] = {10,20,30,145,5}; 
+    printBytes(&test1, 4); 
+    /* 145 > CHAR_MAX */
+    printf("\n");
+
+    unsigned char test2[] = {10,20,30,145,50,30,145}; 
+    printBytes(&test2, -10); 
+    printf("\n");
+
+    printBytes(&test2, 6); 
+    printf("\n");
+
+    
+
+
+    printf("--- Task 2 ---\n");
+    if (argc >= 2) {
+        printf("In file is '%s' and out file is '%s'\n", argv[1], argv[2]);
+    } else {
+        printf("Wrong number of arguments for task 2. At least 2 should be provided\n");
+    }
 
 }
 
@@ -16,8 +38,8 @@ void printBytes(void *ptr, int numBytes) {
 
     int i;
     for (i=1; i<=numBytes; i++) {
-        printf("%03d: %4u\n", i, *(char*)ptr);
-        // Requirement for formatting was unclear, it looked like 4
-        ptr = ptr + 1;
+        printf("%03d: %4hhu\n", i, *(char*)ptr); 
+        /* ^^ The return format type is not defined so I used unsigned char */
+        ptr += 1;
     }
 }
